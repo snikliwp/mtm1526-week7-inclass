4aabf5b72b81b14cbd2b9843c015354942e05b0c
snikliwp <wilk0146@algonquinlive.com>
2012-03-11 19:21:06 -0400

    changrd

--- includes/db.php
+++ includes/db.php
@@ -2,10 +2,10 @@
 
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASSWORD');
-$dns = getenv('DB_DNS');
+$dns = getenv('DB_DSN');
 
 $db = new PDO($dns, $user, $pass);
 
-$db->exec('SET MANES utf8');
+$db->exec('SET NAMES utf8');
 
 require_once 'filter-wrapper.php';
